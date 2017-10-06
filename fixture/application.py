bff3e5a9390435dfc64254ac9fe4d0f4737d0a67
SherMary <30530751+SherMary@users.noreply.github.com>
2017-10-06 09:43:21 +0300

    Не получается залогиниться на почтовый сервер. Код до конца видео 9ого урока.

--- fixture/application.py
+++ fixture/application.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
-
+from fixture.james import JamesHelper
+from fixture.mail import MailHelper
+from fixture.signup import SignupHelper
+from fixture.soap import SoapHelper
 
 class Apllication:
 
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox(capabilities={"marionette": False})
         elif browser == "chrome":
@@ -17,8 +20,13 @@ class Apllication:
             raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
+        self.mail = MailHelper(self)
+        self.signup = SignupHelper(self)
+        self.soap = SoapHelper(self)
+        self.config = config
         #self.wd.implicitly_wait(5)
-        self.base_url = base_url
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
